d792691b42aec5e28552f6756900f345de9e333d
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2022-09-29 10:55:27 +0600

    Font-Awesome Added

--- src/componetns/CardList/CardList.js
+++ src/componetns/CardList/CardList.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Card from '../Card/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHelmetSafety } from '@fortawesome/free-solid-svg-icons';
 
 const CardList = ({cardLists, handleAddToCart}) => {
 
     return (
         <div className='container mt-5 mx-5 text-start'>
-            <h3>Fantasy Club</h3>
+            <div class="d-flex flex-row bd-highlight mb-3">
+                <div><FontAwesomeIcon icon={faHelmetSafety}></FontAwesomeIcon></div>
+                <div className='mx-3'><h3>Fantasy Club</h3></div>
+            </div>
+        
             <h5>Select Today's Collection</h5>
             <br />
             <div className="row row-cols-1 row-cols-md-3 g-4">
